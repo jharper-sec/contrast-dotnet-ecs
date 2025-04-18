846c7732c9f10495bcb9f18eb6e0556e831941f0
Jonathan Harper <39912347+jharper-sec@users.noreply.github.com>
2025-04-18 18:15:57 -0400

    Terraform formatting

--- aws/terraform/ecr.tf
+++ aws/terraform/ecr.tf
@@ -22,9 +22,9 @@ resource "aws_ecr_lifecycle_policy" "demo_policy" {
         rulePriority = 1,
         description  = "Keep only the last 5 images",
         selection = {
-          tagStatus     = "any",
-          countType     = "imageCountMoreThan",
-          countNumber   = 5
+          tagStatus   = "any",
+          countType   = "imageCountMoreThan",
+          countNumber = 5
         },
         action = {
           type = "expire"
